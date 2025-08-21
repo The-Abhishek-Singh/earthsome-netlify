6324a16b365512ea7c2feed81b0db4a02a4acde2
Abhishek Singh <abhi120730@gmail.com>
2025-08-21 14:35:33 +0530

    Update page.js

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -637,7 +637,7 @@ const CartPage = () => {
                         <div className="flex flex-col sm:flex-row sm:justify-between gap-4">
                           <div className="flex-1">
                             <h3 className="font-semibold text-black text-lg mb-1">
-                              {item.name}
+                              {item.productName}
                             </h3>
                             <p className="text-gray-600 text-sm mb-2">
                               {item.description}
