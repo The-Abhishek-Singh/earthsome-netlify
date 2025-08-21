3049502c2bf542aac9829a4d8d69f3450152fe79
Abhishek Singh <abhi120730@gmail.com>
2025-08-21 13:51:05 +0530

    Update page.js

--- src/app/all-product/page.js
+++ src/app/all-product/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import ProductListingPage from "../../Components/Product/Product";
 function page() {
   return (
-    <div>
+    <div className= ' mt-20 mb-[1000px] sm:mb-[700px] md:mb-[600px] lg:mb-[485px] '>
       <ProductListingPage />
     </div>
   );
